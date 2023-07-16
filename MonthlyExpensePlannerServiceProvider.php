1e293b8cc239679787b1743421d7fec466ec037f
azim1993 <azimnurul1993@gmail.com>
2023-07-16 16:55:43 +0600

    Service provider init

---
+++ MonthlyExpensePlannerServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+namespace Azim1993;
+
+use Illuminate\Support\ServiceProvider;
+
+class MonthlyExpensePlannerServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
