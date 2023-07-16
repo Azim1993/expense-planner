5e718d482f16d9f3c37bc20837de5050bca72bcb
azim1993 <azimnurul1993@gmail.com>
2023-07-16 17:17:41 +0600

    Model and migration file created

---
+++ src/Models/Expense.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Azim1993\ExpensePlanner\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expense extends Model
+{
+    use HasFactory;
+}
---
+++ src/Models/MonthlyPlan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Azim1993\ExpensePlanner\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MonthlyPlan extends Model
+{
+    use HasFactory;
+}
