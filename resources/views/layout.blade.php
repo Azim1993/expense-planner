78f1719d7e6328cd35f470ae3dfb0972c1e1123f
azim1993 <azimnurul1993@gmail.com>
2023-07-16 19:01:29 +0600

    Layouts and page base structure done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,1 +1,40 @@
-<?php
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    fontFamily: {
+                        roboto: ['Roboto', 'sans-serif']
+                    },
+                    minHeight: {
+                        'screen-half': '50vh',
+                    },
+                }
+            }
+        }
+    </script>
+</head>
+<body class="bg-white min-w-full min-h-full font-roboto">
+<div class="min-h-screen">
+    <header class="bg-blue-300 py-6 px-6 border-b-2 border-blue-400">
+        <div class="container mx-auto">
+            <h1 class="text-3xl font-bold text-shadow">Monthly Planner</h1>
+        </div>
+    </header>
+    <main class="container border-x py-6 mx-auto min-h-screen-half">
+        @yield('content')
+    </main>
+    <footer class="border-t py-6 px-6 text-center">
+        <p>All right reserved by  &copy; Azim</p>
+    </footer>
+</div>
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,6 @@ use Azim1993\ExpensePlanner\Http\Controllers\MonthlyPlanController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('planner')->group(function() {
-    Route::resource('monthly-plans', MonthlyPlanController::class);
+    Route::resource('monthly-plans', MonthlyPlanController::class, [ 'names' => 'monthly.plans']);
 });
 
