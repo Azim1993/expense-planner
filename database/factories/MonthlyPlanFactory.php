9161d15c74c5bf19cf8ae0d760baf8338907e004
azim1993 <azimnurul1993@gmail.com>
2023-07-16 17:43:56 +0600

    Factory classes generated

---
+++ database/factories/MonthlyPlanFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Azim1993\ExpensePlanner\Factory;
+
+use Azim1993\ExpensePlanner\Models\MonthlyPlan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class MonthlyPlanFactory extends Factory
+{
+    protected $model = MonthlyPlan::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'month' => fake()->month,
+            'year' => fake()->year,
+            'income_amount' => rand(50000, 100000),
+            'targeted_expense_amount' => rand(10000, 50000),
+            'targeted_investment_amount' => rand(5000, 10000),
+            'note' => fake()->text()
+        ];
+    }
+}
