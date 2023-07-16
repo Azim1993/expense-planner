8156aeadc2a158917075757f082f93ca8692001e
azim1993 <azimnurul1993@gmail.com>
2023-07-16 17:49:09 +0600

    Controller created

--- src/MonthlyExpensePlannerServiceProvider.php
+++ src/MonthlyExpensePlannerServiceProvider.php
@@ -8,6 +8,7 @@ class MonthlyExpensePlannerServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     public function register(): void
